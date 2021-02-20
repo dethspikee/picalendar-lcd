b5f4517a9a153690c70d8e959b6c2f4a1023d2b2
Damian Tabor <zettie008@gmail.com>
2021-02-20 20:34:52 +0000

    Write helper function for displaying long messages

--- picalendar/api/show_events.py
+++ picalendar/api/show_events.py
@@ -1,6 +1,43 @@
+from time import sleep
+
 import click
+
+from picalendar import drivers
+from picalendar.api.fetch_events import main as get_events
 
 
 @click.command('show')
 def show():
-    print('Hello world')
+    display = drivers.Lcd()
+    events = get_events()
+    try:
+        while True:
+            display_message(display, 1, events)
+            sleep(5)
+    except KeyboardInterrupt:
+        display.lcd_clear()
+
+
+def display_message(display: drivers.Lcd, row: int, events: list) -> None:
+    """
+    Helper funtion for displaying events on a LCD
+    """
+    COLS = 16
+    for event in events:
+        message = event.get("summary", "")
+        start = event.get("start", "")
+        display.lcd_display_string(f"{start}", 1)
+        if len(message) > COLS:
+            for i in range(0, len(message), COLS):
+                text_to_show = message[i: i + COLS].lstrip()
+                if len(text_to_show) < COLS:
+                    difference = COLS - len(text_to_show)
+                    text_to_show = text_to_show + " " * difference
+                display.lcd_display_string(text_to_show, 2)
+                sleep(2)
+        else:
+            if len(message) < COLS:
+                difference = COLS - len(message)
+                message = message + " " * difference
+            display.lcd_display_string(message, 2)
+            sleep(2)
